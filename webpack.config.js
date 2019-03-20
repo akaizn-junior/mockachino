2da9b1902a6c0ea22e11a6c67e3547b2f26dd50f
Simao Nziaka <sdnziaka@gmail.com>
2019-03-20 00:19:48 -0600

    try to fix security vulnerabilities

--- webpack.config.js
+++ webpack.config.js
@@ -4,39 +4,22 @@ const config = require('./package.json');
 //config the environment
 require('dotenv').config();
 
+const DEV = process.env.NODE_ENV !== 'production';
+
 module.exports = {
     mode: process.env.NODE_ENV,
     entry: path.join(__dirname, config.main),
-    devtool: 'source-map',
+    devtool: DEV ? 'source-map' : '',
     output: {
-        filename: `${process.env.NAME}.min.js`,
-        library: process.env.NAME.charAt(0).toUpperCase() + process.env.NAME.slice(1),
+        filename: DEV ? `${String(process.env.NAME).toLowerCase()}.js` : `${String(process.env.NAME).toLowerCase()}.min.js`,
+        library: process.env.NAME,
         libraryTarget: process.env.TARGET,
-        path: path.join(__dirname, 'dist', process.env.TARGET)
+        path: DEV ? path.join(__dirname, 'dev') : path.join(__dirname, 'dist', process.env.TARGET)
     },
     plugins: [],
     devServer: {
         contentBase: path.join(__dirname, ''),
         port: 7000,
         open: true
-    },
-    module: {
-        //set the rules of transpiling
-        rules: [
-            {
-                //transpile es6 in all the .js files
-                test: /\.js$/,
-                //don't transpile code in the node modules folder
-                exclude: /node_modules/,
-                use: {
-                    //use the babel loader with cache for performance improvements
-                    //cache improves the performance by 2 according to the docs
-                    loader: 'babel-loader?cacheDirectory=true',
-                    options: {
-                        presets: ['env']
-                    }
-                }
-            }
-        ]
     }
 };
