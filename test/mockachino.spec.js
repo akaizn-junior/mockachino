ca734ca1e5360db9628ad503564b85c932af090c
Simao Nziaka <sdnziaka@gmail.com>
2019-10-27 17:08:01 -0600

    trying tests

--- test/mockachino.spec.js
+++ test/mockachino.spec.js
@@ -1,10 +1,15 @@
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
 
-import * as mockachino from '../lib/mockachino';
+import * as mockachino from './tmp/lib/mockachino';
 
 describe('mockachino tests', () => {
-	it('should mock a person', () => {
+	it('should test mock', () => {
 		expect(mockachino.mock).to.be.a('function');
+		expect(mockachino.mock()).has.keys([
+			"person",
+			"phoneNumber",
+			"address"
+		]);
 	});
 });
